1488f067d062538d6f8b86ae96878a07a6df49b1
ADMIN <muhammadmaulanabdz@gmail.com>
2022-05-05 17:03:58 +0700

    loopfun

---
+++ loopfundamental.py
@@ -0,0 +1,10 @@
+
+jumlahbuku = 10
+print("ibu berkata 'Baca Semua bukumu'")
+jumlahbukudibaca = 0
+print(f"saya sudah membaca buku ke {jumlahbukudibaca}")
+
+for jumlahbukudibaca in range(1,jumlahbuku+1) :
+    print(f"saya sudah membaca buku ke {jumlahbukudibaca}")
+
+print(f"bu saya sudah membaca {jumlahbukudibaca} buku")
