bd7779f01aa937b80aa4ac165c8b23903d6e4fd8
ADMIN <muhammadmaulanabdz@gmail.com>
2022-05-05 17:19:26 +0700

    cth loop and update file

---
+++ loopfundamentalwhile.py
@@ -0,0 +1,11 @@
+jumlahbuku = 1000
+print("ibu berkata 'Baca Semua bukumu'")
+jumlahbukudibaca = 0
+print(f"buku yang saya baca adalah {jumlahbukudibaca}")
+
+while jumlahbukudibaca < jumlahbuku :
+
+    jumlahbukudibaca = jumlahbukudibaca + 1
+    print(f"jumlah buku yang saya baca {jumlahbukudibaca} ")
+
+print(f"bu saya sudah membaca {jumlahbukudibaca} buku")
